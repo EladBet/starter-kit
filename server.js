035ea7a6b2f9e665a7415fa16a6a180b71af232c
EldadB <Eladb@playbuzz.vom>
2017-04-09 15:53:28 +0300

    feat(sequelize): get user roles

--- server.js
+++ server.js
@@ -38,39 +38,54 @@ function initDB() {
     host: "node-course.chtl0xl7h64i.us-east-1.rds.amazonaws.com",
     database: "course"
   });
-  //
-  // var Sequelize = require('sequelize');
-  // var sequelize = new Sequelize('couse', 'node4ever', 'pb-node123', {
-  //   host: 'course.cbyydzmmb2nq.eu-central-1.rds.amazonaws.com',
-  //   dialect: 'mysql',
-  //   define: {
-  //     timestamps: false
-  //   },
-  //   pool: {
-  //     max: 5,
-  //     min: 1,
-  //     idle: 10000
-  //   },
-  // });
-  //
-  // const usersModel = sequelize.define('users', {
-  //   id: {type: Sequelize.INTEGER, primaryKey: true},
-  //   name: Sequelize.STRING
-  // }, {
-  //   tableName: 'users'
-  // });
-  //
-  //
-  // sequelize.sync()
-  // .then(() => {
-  //   usersModel.find({where: {name: 'alon'}}).then((result) => {
-  //     console.log(result.name)
-  //   }).catch((err) => {
-  //     console.error(err)
-  //   })
-  // })
-  // .catch((err) => {
-  //   console.error(err);
-  // });
+
+  var Sequelize = require('sequelize');
+  var sequelize = new Sequelize('couse', 'node4ever', 'pb-node123', {
+    host: 'course.cbyydzmmb2nq.eu-central-1.rds.amazonaws.com',
+    dialect: 'mysql',
+    define: {
+      timestamps: false
+    },
+    pool: {
+      max: 5,
+      min: 1,
+      idle: 10000
+    },
+  });
+
+  const usersModel = sequelize.define('users', {
+    id: { type: Sequelize.INTEGER, primaryKey: true },
+    name: Sequelize.STRING
+  }, {
+    tableName: 'users'
+  });
+
+  const rolesModel = sequelize.define('roles', {
+    id: { type: Sequelize.INTEGER, primaryKey: true },
+    type: Sequelize.STRING
+  }, {
+    tableName: 'roles'
+  });
+
+  usersModel.belongsToMany(rolesModel, { through: 'user_role_mapping' });
+  rolesModel.belongsToMany(usersModel, { through: 'user_role_mapping' });
+
+  sequelize.sync().then(() => {
+    usersModel.find({
+      where: { name: 'alon' },
+      include: [{
+        model: rolesModel
+      }]
+    }).then((result) => {
+      console.log('NAME: ' + result.name);
+      console.log('ROLES: ' + result.roles.map(function (role) {
+            return role.type;
+          }));
+    }).catch((err) => {
+      console.error(err);
+    })
+  }).catch((err) => {
+    console.error(err);
+  });
 
 }
